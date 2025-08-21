0c71abfe2cab8a0bb674d2083f5a077ffab7fcd7
HeloSalgado <heloisa.salgado@sptech.school>
2025-08-21 13:56:29 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new("1193232-8738", "Mdei", "8239293", 32);
+nokia.ReceberLigacao();
+nokia.Ligar();
+nokia.InstalarAplicativo("Duolingo");
+
+Console.WriteLine("\nSmartphone Iphone");
+Iphone iphone = new("1198484-4546", "XS", "7343542", 128);
+iphone.ReceberLigacao();
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
